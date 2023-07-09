625d47700cc4456fff6c341c7d967b139a983f97
perdanaph <danangputro9@gmail.com>
2023-07-09 15:29:47 +0700

    feat: create UI  profile page

--- lib/app/modules/home/views/profile_view.dart
+++ lib/app/modules/home/views/profile_view.dart
@@ -1,13 +1,169 @@
 import 'package:flutter/material.dart';
+import '../../../core/themes.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Profile'),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        toolbarHeight: 70,
+        backgroundColor: ThemeApp.white,
+        foregroundColor: ThemeApp.dark,
+        title: const Text(
+          "Profile",
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(25),
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.grey,
+                    image: const DecorationImage(
+                      image: AssetImage(
+                          "assets/page-1/images/removebg-preview-1-raD.png"),
+                    )),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Column(
+                children: [
+                  Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Nama Lengkap",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w700),
+                          ),
+                          TextField(
+                            controller: null,
+                            decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                            ),
+                          ),
+                        ],
+                      )),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Email",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w700),
+                          ),
+                          TextField(
+                            controller: null,
+                            decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                            ),
+                          ),
+                        ],
+                      )),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "No Telepon",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w700),
+                          ),
+                          TextField(
+                            controller: null,
+                            decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                            ),
+                          ),
+                        ],
+                      )),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15,
+                ),
+                decoration: BoxDecoration(
+                  color: ThemeApp.primary,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const InkWell(
+                  onTap: null,
+                  child: Text(
+                    "Simpan",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: ThemeApp.white
+
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15,
+                ),
+                decoration: BoxDecoration(
+                  color: ThemeApp.white,
+                  border: Border.all(color: ThemeApp.dark),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const InkWell(
+                  onTap: null,
+                  child: Text(
+                    "Keluar",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
